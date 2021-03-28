75b6bc897af258db5642a05111c2820b040e5d12
Advaitaaa <73975899+Advaitaaa@users.noreply.github.com>
2021-03-28 15:25:01 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -21,3 +21,78 @@ function player_update()
         });
         canvas.add(player_object);
     }
+}
+
+if(keyPressed == "70" )
+{
+    new_image("ironman_face.png");
+    console.log("f");
+}
+
+if(keyPressed == "66")
+{
+    new_image("spiderman_body.png");
+    console.log("b");
+}
+
+if(keyPressed == "76" )
+{
+    new_image("hulk_legs.png");
+    console.log("l");
+}
+
+if(keyPressed == "82")
+{
+    new_image("thor_right_hand.png");
+    console.log("r");
+}
+
+if(keyPressed == "72")
+{
+    new_image("captain_left_hand.png");
+    console.log("h");
+}
+
+if(keyPressed == "38")
+{
+   function up();
+   console.log("up");
+}
+
+if(keyPressed == "40")
+{
+   function down();
+    console.log("down");
+}
+
+if(keyPressed == "37")
+{
+    function left();
+    console.log("left");
+}
+
+if(keyPressed == "39")
+{
+    function right();
+    console.log("right");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
