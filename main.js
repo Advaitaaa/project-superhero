a6097ef995baf2664befaefaf42bc03720f0e089
Advaitaaa <73975899+Advaitaaa@users.noreply.github.com>
2021-03-28 17:00:27 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -55,28 +55,74 @@ if(keyPressed == "72")
 
 if(keyPressed == "38")
 {
-   function up();
+   up();
    console.log("up");
 }
 
 if(keyPressed == "40")
 {
-   function down();
+    down();
     console.log("down");
 }
 
 if(keyPressed == "37")
 {
-    function left();
+    left();
     console.log("left");
 }
 
 if(keyPressed == "39")
 {
-    function right();
+    right();
     console.log("right");
 }
 
+function up()
+{
+    if(player_y >=0)
+    {
+        player_y = player_y - block_image_height;
+        console.log("block image height = " + block_image_height);
+        console.log("When Up arrow key is pressed, X = " + player_x , " Y = "+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function down()
+{
+    if(player_y <=500)
+    {
+        player_y = player_y + block_image_height;
+        console.log("block image height = " + block_image_height);
+        console.log("When Down arrow key is pressed, X = " + player_x +", Y = " +player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function left()
+{
+    if(player_x >0)
+    {
+        player_x = player_x - block_image_width; 
+        console.log("block image width = " + block_image_width);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function right()
+{
+    if(player_x <=850)
+    {
+        player_x = player_x + block_image_width;
+        console.log("block image width = " + block_image_width);
+        console.log("When Right arrow key is pressed, X = " + player_x +", Y = "+player_y);
+        canvas.remove(player_object);
+        player_update(); 
+    }
+}
 
 
 
